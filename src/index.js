e85c4e69c16642938492917f0b3e9955c7af1a59
Afan Dayu Laksono <afandayul@gmail.com>
2022-06-02 18:52:13 +0700

    conneting to mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri =
+  "mongodb+srv://admin:Laksono07@cluster0.hlnsm9i.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoUri);
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongoDB");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongoDB", err);
+});
 
 app.get("/", (req, res) => {
   res.send("hi world!");
